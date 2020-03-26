5e06e93d1dd0c82c2c5dc1f6723c6d8a7ba82226
Mateusz <jaworski92@gmail.com>
2020-03-26 18:57:36 +0100

    Add project files.

---
+++ Jaworski.BL/Client.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jaworski.BL
+{
+    public class Client
+    {
+        private string _lastName;
+
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int ClientID { get; private set; }
+        
+        public string NameLastName
+        {
+            get
+            {
+                return Name + "." + LastName;
+            }
+        }
+    }
+}
